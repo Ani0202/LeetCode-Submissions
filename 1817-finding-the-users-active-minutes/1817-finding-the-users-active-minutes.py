4a888a883dc38d419531d00db2e82002d0227e8d
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-01-15 13:35:08 +0530

    Time: 729 ms (81.84%), Space: 28 MB (17.78%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        hmap = defaultdict(set)
+        for i,t in logs:
+            hmap[i].add(t)
+        ans = [0 for _ in range(k)]
+        for v in hmap.values():
+            if len(v) <= k:
+                ans[len(v)-1] += 1
+        return ans
+        
