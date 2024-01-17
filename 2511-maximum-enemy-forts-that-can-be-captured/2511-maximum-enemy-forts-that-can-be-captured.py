738d49a883466112d46b4ef92a8d5e3a34de9da7
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-01-17 18:00:25 +0530

    Time: 50 ms (31.05%), Space: 17.4 MB (15.03%) - LeetHub

---
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
@@ -0,0 +1,14 @@
+class Solution:
+    def captureForts(self, forts: List[int]) -> int:
+        n = len(forts)
+        i = 0
+        ans = 0
+        while i < n:
+            j = i + 1
+            if forts[i]:
+                while j < n and forts[j] == 0:
+                    j += 1
+                if j < n and forts[i] + forts[j] == 0:
+                    ans = max(ans, j - i - 1)
+            i = j
+        return ans
