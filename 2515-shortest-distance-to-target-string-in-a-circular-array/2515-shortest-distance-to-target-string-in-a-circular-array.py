12a1c3129ac47e14ddec672c1476ea70d9d17b68
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-02-06 20:34:34 +0530

    Time: 47 ms (98.76%), Space: 16.7 MB (63.78%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,14 +1,10 @@
 class Solution:
     def closetTarget(self, words: List[str], target: str, startIndex: int) -> int:
-        ans = float("inf")
-        temp = startIndex
         n = len(words)
-        while True:
-            if words[temp] == target:
-                print(ans, temp, startIndex)
-                ans = min(ans, abs(temp-startIndex),n-abs(temp-startIndex))
-            temp = (temp+1)%n
-            if temp == startIndex:
-                break
-        return -1 if ans == float("inf") else ans
-        
+        for i in range(n):
+            if (
+                words[(startIndex + i) % n] == target
+                or words[(startIndex - i + n) % n] == target
+            ):
+                return i
+        return -1
