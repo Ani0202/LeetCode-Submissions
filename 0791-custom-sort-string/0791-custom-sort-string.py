30d88fd8874a687451839ba59904e62b325b173a
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-03-12 00:47:11 +0530

    Time: 34 ms (68.03%), Space: 16.5 MB (91.57%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,17 +1,8 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         hmap = Counter()
-        let = set(order)
-        ans = ""
-        for i in s:
-            if i in let:
-                hmap[i] += 1
-            else:
-                ans += i
 
-        for i in order:
-            ans += i*hmap[i]
-        return ans
+        for i, c in enumerate(order):
+            hmap[c] = i
 
-        
-        
+        return "".join(sorted(list(s), key=lambda x: hmap[x]))
