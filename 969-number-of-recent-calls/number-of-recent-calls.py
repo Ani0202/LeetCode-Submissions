4f9be6ad74e0392e2184ebaa393ce2a4ec122793
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-04-28 12:46:41 +0530

    Time: 184 ms (88.66%) | Memory: 22 MB (93.98%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -5,7 +5,7 @@ class RecentCounter:
 
     def ping(self, t: int) -> int:
         self.counter.append(t)
-        while self.counter and self.counter[0] < t - 3000:
+        while self.counter[0] < t - 3000:
             self.counter.popleft()
 
         return len(self.counter)
