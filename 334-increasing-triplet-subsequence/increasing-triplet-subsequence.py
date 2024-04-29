bad9ee52194a9b4d6ed90c8406341cb2b60f66b5
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-04-29 17:13:53 +0530

    Time: 771 ms (92.83%) | Memory: 32.6 MB (29.36%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,14 +1,13 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        n = len(nums)
-        lArr = [float("inf") for _ in range(n)]
-        rArr = [-float("inf") for _ in range(n)]
-        for i in range(1, n):
-            lArr[i] = min(lArr[i - 1], nums[i - 1])
-            rArr[n - i - 1] = max(rArr[n - i], nums[n - i])
-
-        for i in range(n):
-            if lArr[i] < nums[i] < rArr[i]:
+        n1 = float("inf")
+        n2 = float("inf")
+        for num in nums:
+            if num <= n1:
+                n1 = num
+            elif num <= n2:
+                n2 = num
+            else:
                 return True
 
         return False
