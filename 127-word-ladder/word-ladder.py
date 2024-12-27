9862f438ff7db6d3b8a8cb6a734d78f11ebabd90
Aniket Agrawal <agrawalaniket02@gmail.com>
2024-12-27 12:41:25 +0530

    Time: 395 ms (25.33%) | Memory: 18.9 MB (58.24%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        hmap = dict()
+        for word in wordList:
+            hmap[word] = False
+        queue = deque()
+        queue.append((beginWord, 1))
+        while queue:
+            word, count = queue.popleft()
+            for i in range(len(word)):
+                for j in range(26):
+                    newWord = word[:i] + chr(j + 97) + word[i + 1 :]
+                    if newWord in hmap:
+                        if newWord == endWord:
+                            return count + 1
+                        if hmap[newWord] is False:
+                            queue.append((newWord, count + 1))
+                            hmap[newWord] = True
+
+        return 0
