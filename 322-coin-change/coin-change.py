74ff807cbf0591b37c235b0e053acc0d25ed99d8
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-01-03 11:10:58 +0530

    Time: 683 ms (90.37%) | Memory: 18.4 MB (33.59%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,23 +1,9 @@
-from collections import deque
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        visited = set()
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for amt in range(coin, amount + 1):
+                dp[amt] = min(dp[amt], dp[amt - coin] + 1)
 
-        queue = deque()
-        queue.append((amount, 0))
-        while queue:
-            amt, count = queue.popleft()
-            for val in coins:
-                rem = amt - val
-                if rem >= 0 and rem not in visited:
-                    if rem == 0:
-                        return count + 1
-                    else:
-                        visited.add(rem)
-                        queue.append((rem, count + 1))
-
-        return -1
+        return -1 if dp[amount] == float("inf") else dp[amount]
