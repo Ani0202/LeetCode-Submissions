9870fb0d8a2ae6bd2427df5e69d819782cb4c99a
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-01-12 12:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.88%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,18 +4,14 @@ class Solution:
 
         def findParen(oppn, clpn, temp):
             if clpn == n:
-                ans.append(temp[:])
+                ans.append(temp)
                 return
 
             if oppn < n:
-                temp += "("
-                findParen(oppn + 1, clpn, temp)
-                temp = temp[:-1]
+                findParen(oppn + 1, clpn, temp + "(")
 
             if clpn < oppn:
-                temp += ")"
-                findParen(oppn, clpn + 1, temp)
-                temp = temp[:-1]
+                findParen(oppn, clpn + 1, temp + ")")
 
         findParen(0, 0, "")
         return ans
