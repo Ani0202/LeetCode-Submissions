68ce15ceea07f9133b36be5c102273d338cbf221
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-01-21 11:33:59 +0530

    Time: 13 ms (90.77%) | Memory: 18.2 MB (11.42%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -0,0 +1,27 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        def getRowCol(x):
+            row, col = (x - 1) // n, (x - 1) % n
+            if row % 2 == 1:
+                col = n - 1 - col
+            return n - 1 - row, col
+
+        n = len(board)
+        queue = deque([1])
+        visited = {1}
+        ans = 0
+
+        while queue:
+            for _ in range(len(queue)):
+                x = queue.popleft()
+                for y in range(x + 1, min(x + 7, n * n + 1)):
+                    i, j = getRowCol(y)
+                    if board[i][j] != -1:
+                        y = board[i][j]
+                    if y not in visited:
+                        if y == n * n:
+                            return ans + 1
+                        queue.append(y)
+                        visited.add(y)
+            ans += 1
+        return -1
