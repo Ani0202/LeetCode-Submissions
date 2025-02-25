6d01e3a44aaed10a3f72130d6884294c0d7c5b57
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-02-25 12:24:06 +0530

    Time: 58 ms (69.71%) | Memory: 34.4 MB (30.78%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,35 +1,11 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        min1 = float("inf")
-        min2 = float("inf")
-        max1 = -float("inf")
-        max2 = -float("inf")
-        minInd1 = -1
-        minInd2 = -1
-        maxInd1 = -1
-        maxInd2 = -1
-        for ind, array in enumerate(arrays):
-            minArr = min(array)
-            maxArr = max(array)
-            if minArr <= min1:
-                min2 = min1
-                minInd2 = minInd1
-                min1 = minArr
-                minInd1 = ind
-            elif minArr <= min2:
-                min2 = minArr
-                minInd2 = ind
+        prevMax = arrays[0][-1]
+        prevMin = arrays[0][0]
+        ans = -1
+        for array in arrays[1:]:
+            ans = max(ans, array[-1] - prevMin, prevMax - array[0])
+            prevMin = min(prevMin, array[0])
+            prevMax = max(prevMax, array[-1])
 
-            if maxArr >= max1:
-                max2 = max1
-                maxInd2 = maxInd1
-                max1 = maxArr
-                maxInd1 = ind
-            elif maxArr >= max2:
-                max2 = maxArr
-                maxInd2 = ind
-
-        if maxInd1 != minInd1:
-            return max1 - min1
-        else:
-            return max(max1 - min2, max2 - min1)
+        return ans
