71455762fa99ab6108864acec9305802e181d672
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-08-02 11:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (23.38%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -1,4 +1,15 @@
 class Solution:
+    MOD = 10**9 + 7
+
+    def power(self, x, n):
+        if n == 0:
+            return 1
+
+        res = self.power(x, n // 2)
+        if n % 2 == 0:
+            return (res * res) % self.MOD
+        else:
+            return (x * res * res) % self.MOD
+
     def monkeyMove(self, n: int) -> int:
-        mod = 10**9 + 7
-        return (pow(2, n, mod) - 2) % mod
+        return (self.power(2, n) - 2) % self.MOD
