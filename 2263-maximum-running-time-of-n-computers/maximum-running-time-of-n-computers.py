d82fb813f4693d9133c465eec911aaedaae1e1a3
Aniket Agrawal <agrawalaniket02@gmail.com>
2025-12-01 14:34:32 -0600

    Time: 1799 ms (11.71%) | Memory: 30.8 MB (97.07%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -6,13 +6,10 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
             total_charge = 0
-            charge_needed = n * mid
             for charge in batteries:
                 total_charge += min(mid, charge)
-                if total_charge >= charge_needed:
-                    break
 
-            if total_charge >= charge_needed:
+            if total_charge >= n * mid:
                 ans = mid
                 low = mid + 1
             else:
