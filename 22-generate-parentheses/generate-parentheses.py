3a930152c11ec9c27606144cc690a58f1b9e87fe
Aniket Agrawal <agrawalaniket02@gmail.com>
2026-03-24 18:57:37 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (97.54%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,16 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.ans = []
+        ans = []
+        temp = []
 
         def build_par(temp, o, c):
             if c == 0:
-                self.ans.append(temp)
+                ans.append("".join(temp))
                 return
 
             if o != 0:
-                build_par(temp + "(", o - 1, c)
+                temp.append("(")
+                build_par(temp, o - 1, c)
+                temp.pop()
             if c > o:
-                build_par(temp + ")", o, c - 1)
+                temp.append(")")
+                build_par(temp, o, c - 1)
+                temp.pop()
 
-        build_par("", n, n)
-        return self.ans
+        build_par(temp, n, n)
+        return ans
