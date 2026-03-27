5499f0115756eec535930583c8a9c0b978b24dbe
Aniket Agrawal <agrawalaniket02@gmail.com>
2026-03-27 12:39:44 -0500

    Time: 0 ms (100.00%) | Memory: 21 MB (97.01%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
+        xor = 0
         for num in nums:
-            ans ^= num
+            xor ^= num
 
-        return ans
+        return xor
