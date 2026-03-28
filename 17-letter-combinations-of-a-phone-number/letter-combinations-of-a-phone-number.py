c2178c1cb5fa743c2f7551ae421f1f671d6cedaa
Aniket Agrawal <agrawalaniket02@gmail.com>
2026-03-28 11:41:39 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.06%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        ans = []
+        hmap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backTrack(digits, tempAns):
+            if digits == "":
+                ans.append(tempAns)
+            else:
+                for letter in hmap[digits[0]]:
+                    backTrack(digits[1:], tempAns + letter)
+
+        backTrack(digits, "")
+
+        return ans
